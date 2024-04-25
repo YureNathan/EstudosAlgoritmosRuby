4fb032fb254bf3e9f83f3e6dd2c00f5874121c41
Yure N <yure.nathan303@gmail.com>
2024-04-25 12:49:45 -0300

    my fist commit

---
+++ strings.rb
@@ -0,0 +1,31 @@
+# strings 
+
+# var = "Yure Nathan"
+
+# mensagem = "Olá #{var}"
+
+# puts mensagem
+
+
+# Herodoc 
+
+# nome = "Yure"
+
+# mensagem = <<~TEXT
+#   "Olá #{nome} bem vindo!"
+# TEXT
+# puts mensagem
+
+
+# name = "Yure"
+
+# mensagem = %Q(bem vindo #{name} ao meu programa!)
+
+# puts mensagem
+
+
+name = "name name"
+
+mensagem = %Q(o tamanho string é #{name.length})
+
+puts mensagem
